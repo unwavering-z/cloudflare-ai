186fc6f174fc82e04c39e19342d888af3e09ef9e
unwavering-z <morehope586@gmail.com>
2026-01-02 22:13:11 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    runtime: 'edge', 
+  },
   output: "standalone",
   typedRoutes: true,
   reactCompiler: true,
