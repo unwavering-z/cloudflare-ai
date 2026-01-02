0178374e741c773a55bf8501c193d95f204b747a
unwavering-z <morehope586@gmail.com>
2026-01-02 23:02:09 +0800

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,8 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function proxy(request: NextRequest) {
-  if (!process.env.APP_PASSWORD) {
-    return NextResponse.next();
-  }
-  const password = request.headers.get("Authorization");
-  if (password !== process.env.APP_PASSWORD) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
 }
 
-export const config = {
-  matcher: "/api/:path*",
-};
+export const runtime = 'edge';
